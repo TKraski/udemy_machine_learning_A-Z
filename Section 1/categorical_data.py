be154a57be0473d164f403cca951161ea43cf5c6
Timo Kraski <timo.kraski@wortmann.com>
2019-09-12 11:54:53 +0200

    Section 8

--- Section 1/categorical_data.py
+++ Section 1/categorical_data.py
@@ -20,7 +20,7 @@ X[:, 1:3] = imputer.transform(X[:, 1:3])
 # Encoding the Independent Variable
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
-X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+X[:, 1] = labelencoder_X.fit_transform(X[:, 1])
 onehotencoder = OneHotEncoder(categorical_features = [0])
 X = onehotencoder.fit_transform(X).toarray()
 # Encoding the Dependent Variable
